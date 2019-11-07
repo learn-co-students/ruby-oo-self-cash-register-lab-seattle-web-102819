b4fb96d2f3aa99856fcc5f2301ff6de90f60c941
github username <github email address>
2019-11-06 16:11:48 -0800

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
 
+class CashRegister
+ 
+ attr_accessor :discount, :total, :item
+  @@all []
+  
+  def initialize (discount=0)
+      @total = 0
+      @discount= 20
+      @@all << self
+   
+    end
+   
+   def self.all
+    @@all
+  end
+   
+  def total
+     @total
+  end
+ 
+  def add_item(title, amount, quantity=1)
+     self.total += amount * quantity
+         quantity.times do
+         items << title
+     end
+     self.last_transaction = amount * quantity
+  end
+
+end
